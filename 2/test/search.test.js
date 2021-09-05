a134c7e7ee6a972f7a5d402423bf4e5d79ae5d6d
Andre Setiawan <andresetiawan136@gmail.com>
2021-09-05 16:31:23 +0700

    some unit test

--- 2/test/search.test.js
+++ 2/test/search.test.js
@@ -1,15 +1,57 @@
-var {expect} = require('chai');
+var chai = require('chai');
+var expect = require('chai').expect;
+var chai_http = require('chai-http');
 var axios = require('axios');
 
+chai.use(chai_http);
+
 describe("Search Test", ()=> {
-  var url = `http://localhost:8001/apis/search`;
-  it("Search with String A-Z", async ()=> {
-    let request = await axios.get(url, {
-      params: {
-        "s": "Batman",
-        "page": 1
-      }
+  var url = `http://localhost:8001`;
+  
+  it("Search with String A-Z",  (done)=> {
+    chai.request(url)
+    .get(`/apis/search`)
+    .query({s: "Batman", page: "1"})
+    .end((err, res) => {
+      //console.log(res)
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      expect(res.body.status).to.equal("success");
+      done()
     })
-    expect(1+1).to.equal(2);
+    // expect(request.data.status).to.equal("success");
+    // expect(request.data.data.Search).to.equal()
+  });
+  it("Search with String A-Z, without page",  (done)=> {
+    chai.request(url)
+    .get(`/apis/search`)
+    .query({s: "Batman"})
+    .end((err, res) => {
+      //console.log(res)
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      expect(res.body).to.be.a('object');
+      expect(res.body.status).to.equal("success");
+      expect(res.body.page).to.equal(1);
+      done()
+    })
+    // expect(request.data.status).to.equal("success");
+    // expect(request.data.data.Search).to.equal()
+  });
+  it("Search with String A-Z, without page",  (done)=> {
+    chai.request(url)
+    .get(`/apis/search`)
+    .query({s: "Batman"})
+    .end((err, res) => {
+      //console.log(res)
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      expect(res.body).to.be.a('object');
+      expect(res.body.status).to.equal("success");
+      expect(res.body.page).to.equal(1);
+      done();
+    })
+    // expect(request.data.status).to.equal("success");
+    // expect(request.data.data.Search).to.equal()
   });
 })
